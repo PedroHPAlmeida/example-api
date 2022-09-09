03e038cf212d2a1093c5d3e4fe2c6243d33f8a45
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-09-09 14:49:22 -0300

    feat: adicionando arquivo para popular o banco de dados com valores iniciais (apenas ambiente local/testes)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO CLIENTES(nome, data_nascimento, sexo) VALUES ('Pedro Henrique', '2001-02-09', 'm');
+INSERT INTO CLIENTES(nome, data_nascimento, sexo) VALUES ('José Almeida', '2000-03-16', 'm');
+INSERT INTO CLIENTES(nome, data_nascimento, sexo) VALUES ('Maria Pereira', '1990-07-29', 'm');
+INSERT INTO CLIENTES(nome, data_nascimento, sexo) VALUES ('João Silva', '2002-02-02', 'm');
+INSERT INTO CLIENTES(nome, data_nascimento, sexo) VALUES ('Ana Marques', '1995-12-31', 'm');
+INSERT INTO CLIENTES(nome, data_nascimento, sexo) VALUES ('Joana Lacerda', '1978-09-10', 'm');
+INSERT INTO CLIENTES(nome, data_nascimento, sexo) VALUES ('Lucas Mendes', '1980-01-01', 'm');
+INSERT INTO CLIENTES(nome, data_nascimento, sexo) VALUES ('Jorge Oliveira', '1982-04-17', 'm');
+INSERT INTO CLIENTES(nome, data_nascimento, sexo) VALUES ('Mateus Ferreira', '1966-12-22', 'm');
+INSERT INTO CLIENTES(nome, data_nascimento, sexo) VALUES ('Gustavo Azevedo', '1969-08-24', 'm');
