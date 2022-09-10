8191b10da8d5f055a40ec244bc5eefd1a5ce60c1
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-09-10 11:10:21 -0300

    feat(SECURITY): ajustes finais nas configurações de segurança para implementar autenticação via Token JWT

--- src/main/java/com/example/demo/config/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/security/SecurityConfiguration.java
@@ -1,13 +1,31 @@
 package com.example.demo.config.security;
 
+import com.example.demo.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioService usuarioService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -15,8 +33,25 @@ public class SecurityConfiguration {
                 .antMatchers(HttpMethod.GET, "/api/clientes").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/clientes/*").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/clientes").permitAll()
-                .anyRequest().authenticated();
-        http.headers().frameOptions().sameOrigin();
+                .antMatchers(HttpMethod.POST, "/api/auth").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioService), UsernamePasswordAuthenticationFilter.class); // Faz com que o nosso filtro rode antes de autenticar o usuário
+
         return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticacaoService).passwordEncoder(passwordEncoder());
+    }
+
 }
