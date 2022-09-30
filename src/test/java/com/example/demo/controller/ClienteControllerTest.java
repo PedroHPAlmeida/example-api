c40e4941eaa83e3282fca6a4528f4754e6892cf0
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-09-29 21:19:51 -0300

    test(TEST): teste do método salvar da classe ClienteController

---
+++ src/test/java/com/example/demo/controller/ClienteControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class ClienteControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void deveriaDevolver200ComOsDadosDeClientesCorretos() throws Exception {
+        URI uri = new URI("/api/clientes");
+        String json = "{\"nome\": \"Cliente\", \"dataNascimento\": \"2000-01-01\", \"sexo\": \"m\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(uri)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(201));
+    }
+}
