1efd2b60308fb28af8d6022302db585a982b224b
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-10-28 21:09:18 -0300

    test(TEST): testando classe ProdutoRepository

---
+++ src/test/java/com/example/demo/repository/ProdutoRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Produto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class ProdutoRepositoryTest {
+
+    @Autowired
+    private IProdutoRepository produtoRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    public void salvarProduto() {
+        Produto produto = new Produto();
+        produto.setNome("Produto 1");
+        produto.setPreco(new BigDecimal("10.0"));
+        entityManager.persist(produto);
+    }
+
+    @Test
+    void deveriaCarregarUmProdutoAoBuscarPeloSeuId() {
+        salvarProduto();
+        Optional<Produto> produto = produtoRepository.findById(1L);
+        assertTrue(produto.isPresent());
+        assertEquals("Produto 1", produto.get().getNome());
+    }
+}
